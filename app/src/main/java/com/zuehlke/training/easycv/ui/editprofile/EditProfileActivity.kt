d9af0f1dd486917f8da957b8b41e7cee2dca8227
Frei, Jasmin <jasmin.frei@zuehlke.com>
2020-03-10 12:29:24 +0100

    made up navigation possible

--- app/src/main/java/com/zuehlke/training/easycv/ui/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/zuehlke/training/easycv/ui/editprofile/EditProfileActivity.kt
@@ -2,14 +2,32 @@ package com.zuehlke.training.easycv.ui.editprofile
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.zuehlke.training.easycv.R
 
+
 class EditProfileActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
-        setTitle(R.string.title_edit_profile)
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration.Builder()
+            .setFallbackOnNavigateUpListener {
+                // Trigger the Activity's navigate up functionality
+                super.onSupportNavigateUp()
+            }.build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navigateUp(navController, appBarConfiguration);
     }
 
 }
