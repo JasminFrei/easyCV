67c8bd832014614aad96fa28342ac291a3cbca3e
Frei, Jasmin <jasmin.frei@zuehlke.com>
2020-03-11 10:14:33 +0100

    added a layout for the Addressfragment

--- app/src/androidTest/java/com/zuehlke/training/easycv/ui/editprofile/AddressFragmentTest.kt
+++ app/src/androidTest/java/com/zuehlke/training/easycv/ui/editprofile/AddressFragmentTest.kt
@@ -3,7 +3,7 @@ package com.zuehlke.training.easycv.ui.editprofile
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
@@ -16,9 +16,12 @@ import org.junit.runner.RunWith
 class AddressFragmentTest {
 
     @Test
-    fun testInitialText() {
+    fun testContent_NoData() {
         launchFragmentInContainer<AddressFragment>(themeResId = R.style.AppTheme)
 
-        onView(withText("Edit Address")).check(matches(isDisplayed()))
+        onView(withId(R.id.txtStreet)).check(matches(withText("")))
+        onView(withId(R.id.txtZip)).check(matches(withText("")))
+        onView(withId(R.id.txtCity)).check(matches(withText("")))
+        onView(withId(R.id.txtCountry)).check(matches(withText("")))
     }
 }
