29f8069a8262f62102eee5de244cb3a181ce1ed6
Frei, Jasmin <jasmin.frei@zuehlke.com>
2020-03-13 11:41:33 +0100

    formatting

--- app/src/main/java/com/zuehlke/training/easycv/ui/NavigationExtension.kt
+++ app/src/main/java/com/zuehlke/training/easycv/ui/NavigationExtension.kt
@@ -7,42 +7,42 @@ import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 
 
-    /**
-     * Checks if the chosen Activity in the intent matches with the destination of the given
-     * direction
-     */
-    private fun Fragment.isNavigationLegal(intent: Intent?, direction: NavDirections): Boolean {
-        val className = intent?.component?.className
-        val actionId = direction.actionId
-        val navAction = findNavController().currentDestination?.getAction(actionId)
-        val destinationId = navAction?.destinationId
-        if (destinationId != null) {
-            val destination = findNavController().graph.findNode(destinationId)
-            if (destination is ActivityNavigator.Destination) {
-                if (destination.component?.className == className) {
-                    return true
-                }
+/**
+ * Checks if the chosen Activity in the intent matches with the destination of the given
+ * direction
+ */
+private fun Fragment.isNavigationLegal(intent: Intent?, direction: NavDirections): Boolean {
+    val className = intent?.component?.className
+    val actionId = direction.actionId
+    val navAction = findNavController().currentDestination?.getAction(actionId)
+    val destinationId = navAction?.destinationId
+    if (destinationId != null) {
+        val destination = findNavController().graph.findNode(destinationId)
+        if (destination is ActivityNavigator.Destination) {
+            if (destination.component?.className == className) {
+                return true
             }
         }
-        return false
     }
+    return false
+}
 
 fun Fragment.navigateToActivity(intent: Intent?, direction: NavDirections) {
-        if (isNavigationLegal(intent, direction)) {
-            startActivity(intent)
-        } else {
-            throw IllegalArgumentException()
-        }
+    if (isNavigationLegal(intent, direction)) {
+        startActivity(intent)
+    } else {
+        throw IllegalArgumentException()
     }
+}
 
 fun Fragment.navigateToActivityForResult(
     intent: Intent?,
     direction: NavDirections,
     requestCode: Int
 ) {
-        if (isNavigationLegal(intent, direction)) {
-            startActivityForResult(intent, requestCode)
-        } else {
-            throw IllegalArgumentException()
-        }
+    if (isNavigationLegal(intent, direction)) {
+        startActivityForResult(intent, requestCode)
+    } else {
+        throw IllegalArgumentException()
     }
+}
